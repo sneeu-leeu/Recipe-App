acf1f2dd62391c1cfea7618457e503e246f49e43
luftedar <ugurorcun91@gmail.com>
2022-03-14 22:52:19 +0300

    Generate Recipes Controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "food#index"
+  root to: "recipe#index"
 
   resources :foods, :recipes, :public_recipes, :shopping_list
 end
