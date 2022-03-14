6f941aa440aabfc8afd1a91b6f75380771d92f7f
luftedar <ugurorcun91@gmail.com>
2022-03-14 23:05:25 +0300

    Fix Root Route Typeo

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "recipe#index"
+  root to: "recipes#index"
 
   resources :foods, :recipes, :public_recipes, :shopping_list
 end
