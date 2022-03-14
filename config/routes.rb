e66177deb3638f0d99e3fec4fe80ff47bbf9fda1
luftedar <ugurorcun91@gmail.com>
2022-03-14 22:38:46 +0300

    Add Routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "food#index"
+
+  resources :foods, :recipes, :public_recipes, :shopping_list
 end
