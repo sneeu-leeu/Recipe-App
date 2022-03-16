05794440461e446b640f7a89bb50b2e7597115ad
luftedar <ugurorcun91@gmail.com>
2022-03-16 11:11:04 +0300

    Add CRUD actions for Recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,16 +1,39 @@
 class RecipesController < ApplicationController
   def index
+    @recipes = Recipe.all
   end
 
   def show
+    @recipe = Recipe.find_by(params[:id])
   end
 
   def new
+    @recipe = Recipe.new
   end
 
   def create
+    @new_recipe = current_user.recipe.new(recipe_params)
+    @recipe.user_id = current_user.id
+    if @recipe.save
+      flash[:notice] = 'Recipe successfully created'
+      redirect_to @recipe
+    else
+      flash[:notice] = 'Something went wrong'
+      render 'new'
+    end
   end
 
   def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    flash[:notice] = 'The recipe was successfully destroyed.'
+    redirect_to recipes_url
+  end
+
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :cookingTime, :preparationTime, :description, :public)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@
 
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "recipes#index"
 
   resources :foods, :recipes, :public_recipes, :shopping_list
