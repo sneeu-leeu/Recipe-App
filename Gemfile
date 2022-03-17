270ff25e26e6911aa1ad8004c3f4015794a6f0c5
sneeu-leeu <sa070197@gmail.com>
2022-03-17 12:19:36 +0200

    create foods page, controllers and formsms

--- Gemfile
+++ Gemfile
@@ -23,6 +23,8 @@ gem 'jbuilder', '~> 2.7'
 # gem 'bcrypt', '~> 3.1.7'
 gem 'cancancan'
 gem 'devise'
+gem 'jquery-ui-rails'
+gem 'bootstrap', '~> 5.1.3'
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "foods#index"
 
-  resources :foods, only: %i[index]
+  resources :foods, only: [:index, :new, :create, :destroy]
 end
