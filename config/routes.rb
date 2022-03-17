f54d7869477786597ecbba2b0b9518fd7fd1d443
luftedar <ugurorcun91@gmail.com>
2022-03-17 16:02:14 +0300

    Update Routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "foods#index"
 
-  resources :foods, only: [:index, :new, :create, :destroy]
-
-  resources :public_recipes, :shopping_list
-
-  resources :recipes do 
-    resources :recipe_foods
-  end 
+  resources :users, only: %i[index] 
+  resources :foods, only: %i[index new create destroy]
+  resources :recipes, only: [:index, :show, :new, :create, :destroy] do
+    resources :recipe_foods, only: [:new, :create, :destroy, :update, :edit]
+    resources :shopping_list, only: [:index ,:show]
+  end
+  resources :inventories do
+    resources :inventory_foods
+  end
 end
