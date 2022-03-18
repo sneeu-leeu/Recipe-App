21ed3af5176386e39094af1603221d78963adfe5
sneeu-leeu <sa070197@gmail.com>
2022-03-18 18:51:42 +0200

    Add Bootstrap to recipes.erb

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :foods, dependant: :destroy
+  has_many :foods, dependent: :destroy
   has_many :recipes, dependent: :destroy
 
   validates :name, presence: true
