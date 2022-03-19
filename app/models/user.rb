b60a7ec5edc46d42f9d73446115d2d3bed80ab63
sneeu-leeu <sa070197@gmail.com>
2022-03-19 14:48:17 +0200

    create header partial and fix authorize

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,23 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  after_create :set_role
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  # validates :name, presence: true, length: { minimum: 3 }
+
+  validates :name, presence: true, length: { minimum: 3 }
+
   has_many :foods, dependent: :destroy
   has_many :recipes, dependent: :destroy
   has_many :inventories, dependent: :destroy
 
   def set_role
     update(role: 'user')
+  end
+
+  def admin?
+    false
   end
 
   def sign_out
